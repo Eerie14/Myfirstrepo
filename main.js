9f85bc6f21611d238e981130c3e915e0e685f42c
Eerie14 <donahi.romero@gmail.com>
2024-07-13 00:01:47 +0000

    popup

---
+++ main.js
@@ -0,0 +1,30 @@
+function popup() {
+    var x = document.getElementById("popcontainer");
+    if (x.style.display==="none"){
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function closepopup() {
+    var x = document.getElementById("popcontainer")
+    if (x.style.display = "block"){
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+/*function slider() {
+    var x = document.getElementById("reel");
+    if (x.style.display==="block"){
+        x.style.transform="width=100px"
+    }
+    else {
+        x.style.transform="width=200px" 
+    }
+}*/
+function responsive() {
+    var x
+}
